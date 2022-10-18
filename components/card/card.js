95928935903ccef8089d87c16dd49329feaf0738
Roger Pender <rdpender3@gmail.com>
2022-10-18 14:49:02 -0700

    adding default error img

--- components/card/card.js
+++ components/card/card.js
@@ -1,20 +1,32 @@
 import Image from "next/image";
+import { useState } from "react";
 import styles from './card.module.css';
 
 const Card = (props) => {
 
-    const { imgUrl, size } = props;
+    const {
+        imgUrl = 'https://images.unsplash.com/photo-1485846234645-a62644f84728?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1340&q=80',
+        size = 'medium' 
+    } = props;
+
+    const [imgSrc, setImgSrc] = useState(imgUrl);
 
     const sizeMap = {
         large: styles.lgItem,
         medium: styles.mdItem,
         small: styles.smItem,
-      };
+    };
+
+    const handleError = () => {
+        setImgSrc(
+            "https://images.unsplash.com/photo-1485846234645-a62644f84728?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1340&q=80"
+        );
+    }
 
     return(
         <div className={styles.container}>
             <div className={sizeMap[size]}>
-                <Image src={imgUrl} alt='card image' layout="fill" className={styles.cardImg} />
+                <Image src={imgSrc} alt='card image' layout="fill" className={styles.cardImg} onError={handleError} />
             </div>
         </div>
     )
